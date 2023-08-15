a34adc2712dc2ca9f01ee243777d4946651215bc
emesan22 <emesan@mail.emesan.click>
2023-08-15 16:47:48 +0900

    🥵
    - コンポーネントを追加
      - header
      - footer
      - など
    - フォントをLINE Seed JPに
    - どうにかハンバーガーメニューにした (cssまだだけど...)
    - publick化に応じてpackage.jsonを変更
    - robots.txtを変更(サイトマップ)
    - ファビコンの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,29 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "../components/footer";
+import Link from "next/link";
+import Header from "../components/header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Mynecraft',
-  description: '好きなことを、楽しく',
+  title: "Mynecraft",
+  description: "好きなことを、楽しく",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
-      <body className={inter.className}>
-        <Header/>
-        <main className='min-h-screen'>
-          {children}
-        </main>
-        <Footer/>
+    <html lang="ja" className="font-main">
+      <body className="font-main">
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Card from "../components/card";
 export default function Home() {
   return (
     <>
-      <div className="main-top">
-        <span className="text-4xl">
+      <div className="main-top flex flex-col flex-wrap content-center justify-center h-80">
+        <span className="text-8xl">
           好きなことを、
           <br />
           楽しく。
         </span>
         <p>そのようなスローガンを元に、私達は開発します。</p>
       </div>
-      <div className="inner">
+      <div className="inner flex ">
         <Card title="News" />
         <Card title="Works" description="私達はこちらの作品を作っています。" />
       </div>
